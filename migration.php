2494b8c3ea705f0a45a56a15c1e3d9b6213d4a56
Shivanand Sharma <shivanand@binaryturf.com>
2023-04-16 11:31:51 +0530

    testing all export formats

--- migration.php
+++ migration.php
@@ -3,6 +3,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	class T_LD_Mig {
 		function __construct() {
@@ -12,7 +13,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			// Tutor 		=> Course > Topic > Lesson
 			// LearnDash	=> Course > Lesson > Topic
 			$this->export_courses_to_courses(); // Courses are mapped to courses
-			$this->export_topics_to_lessons(); // Tutor Topics are mapped to Lessons
+			$this->export_topics_to_lesson(); // Tutor Topics are mapped to Lessons
 			$this->export_lessons_to_topics(); // Tutor Lessons are mapped to topics
 			$this->export_enrollments();
 		}
@@ -26,7 +27,6 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			);
 
 			if ( $courses && $courses->post_count ) {
-
 				$courses = $courses->posts;
 				foreach ( $courses as $course ) {
 					$post_meta              = get_post_meta( $course->ID );
@@ -46,39 +46,40 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 					$product_id    = $post_meta['_tutor_course_product_id'];
 					$product       = get_post_meta( $product_id, '_price', true );
 					$product_price = $product;// ->get_price();
-					$_sfwd_courses = array(
-						'0'                                => '',
-						'sfwd-courses_certificate'         => '',
-						'sfwd-courses_course_disable_content_table' => '',
-						'sfwd-courses_course_disable_lesson_progression' => 'on',
-						'sfwd-courses_course_lesson_order_enabled' => '',
-						'sfwd-courses_course_lesson_order' => 'ASC',
-						'sfwd-courses_course_lesson_orderby' => 'menu_order',
-						'sfwd-courses_course_lesson_per_page_custom' => '',
-						'sfwd-courses_course_lesson_per_page' => '',
-						'sfwd-courses_course_materials_enabled' => '',
-						'sfwd-courses_course_materials'    => '',
-						'sfwd-courses_course_points_access' => '',
-						'sfwd-courses_course_points_enabled' => '',
-						'sfwd-courses_course_points'       => '',
-						'sfwd-courses_course_prerequisite_compare' => 'ANY',
-						'sfwd-courses_course_prerequisite_enabled' => '',
-						'sfwd-courses_course_prerequisite' => '',
-						'sfwd-courses_course_price_billing_p3' => '',
-						'sfwd-courses_course_price_billing_t3' => '',
-						'sfwd-courses_course_price_type_paynow_enrollment_url' => '',
-						'sfwd-courses_course_price_type'   => $price_type,
-						'sfwd-courses_course_price'        => $product_price,
-						'sfwd-courses_course_topic_per_page_custom' => '',
-						'sfwd-courses_course_trial_duration_p1' => '',
-						'sfwd-courses_course_trial_duration_t1' => '',
-						'sfwd-courses_course_trial_price'  => '',
-						'sfwd-courses_custom_button_url'   => '',
-						'sfwd-courses_exam_challenge'      => 0,
-						'sfwd-courses_expire_access_days'  => 0,
-						'sfwd-courses_expire_access_delete_progress' => '',
-						'sfwd-courses_expire_access'       => '',
-					);
+					$_sfwd_courses =
+						array(
+							'0'                           => '',
+							'sfwd-courses_certificate'    => '',
+							'sfwd-courses_course_disable_content_table' => '',
+							'sfwd-courses_course_disable_lesson_progression' => 'on',
+							'sfwd-courses_course_lesson_order_enabled' => '',
+							'sfwd-courses_course_lesson_order' => 'ASC',
+							'sfwd-courses_course_lesson_orderby' => 'menu_order',
+							'sfwd-courses_course_lesson_per_page_custom' => '',
+							'sfwd-courses_course_lesson_per_page' => '',
+							'sfwd-courses_course_materials_enabled' => '',
+							'sfwd-courses_course_materials' => '',
+							'sfwd-courses_course_points_access' => '',
+							'sfwd-courses_course_points_enabled' => '',
+							'sfwd-courses_course_points'  => '',
+							'sfwd-courses_course_prerequisite_compare' => 'ANY',
+							'sfwd-courses_course_prerequisite_enabled' => '',
+							'sfwd-courses_course_prerequisite' => '',
+							'sfwd-courses_course_price_billing_p3' => '',
+							'sfwd-courses_course_price_billing_t3' => '',
+							'sfwd-courses_course_price_type_paynow_enrollment_url' => '',
+							'sfwd-courses_course_price_type' => $price_type,
+							'sfwd-courses_course_price'   => $product_price,
+							'sfwd-courses_course_topic_per_page_custom' => '',
+							'sfwd-courses_course_trial_duration_p1' => '',
+							'sfwd-courses_course_trial_duration_t1' => '',
+							'sfwd-courses_course_trial_price' => '',
+							'sfwd-courses_custom_button_url' => '',
+							'sfwd-courses_exam_challenge' => 0,
+							'sfwd-courses_expire_access_days' => 0,
+							'sfwd-courses_expire_access_delete_progress' => '',
+							'sfwd-courses_expire_access'  => '',
+						);
 
 					if ( $price_type == 'subscribe' ) {
 						$_sfwd_courses['sfwd-courses_course_price_billing_cycle'] = '';
@@ -93,6 +94,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 							'ID'                    => $course->ID,
 							'post_author'           => $course->post_author,
 							'post_date'             => $course->post_date,
+							'post_date_gmt'         => $course->post_date_gmt,
 							'post_content'          => $course->post_content,
 							'post_title'            => $course->post_title,
 							'post_excerpt'          => $course->post_excerpt,
@@ -113,10 +115,10 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 							'comment_count'         => '0',
 							'filter'                => 'raw',
 						),
-						'wp_post_permalink' => get_permalink( $course->ID ),
+						'wp_post_permalink' => trailingslashit( home_url() . '/courses/' . $course->post_name ), // get_permalink( $course->ID ), // trailingslashit( home_url() . '/topics/' . $lesson->post_name ),
 						'wp_post_meta'      => array(
-							'_ld_price_type' => $price_type,
-							'_sfwd-courses'  => $_sfwd_courses,
+							'_ld_price_type' => array( $price_type ),
+							'_sfwd-courses'  => array( (object) $_sfwd_courses ),
 						),
 						'wp_post_terms'     => array(
 							'ld_course_category' => array(),
@@ -143,6 +145,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 							'ID'                    => $topic->ID,
 							'post_author'           => $topic->post_author,
 							'post_date'             => $topic->post_date,
+							'post_date_gmt'         => $topic->post_date_gmt,
 							'post_content'          => $topic->post_content,
 							'post_title'            => $topic->post_title,
 							'post_excerpt'          => $topic->post_excerpt,
@@ -163,7 +166,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 							'comment_count'         => '0',
 							'filter'                => 'raw',
 						),
-						'wp_post_permalink' => get_permalink( $topic->ID ),
+						'wp_post_permalink' => trailingslashit( home_url() . '/lessons/' . $topic->post_name ),
 						'wp_post_meta'      => array(
 							'course_id'     => array( wp_get_post_parent_id( $topic->ID ) ),
 							'_sfwd-lessons' => array(
@@ -182,8 +185,95 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 				}
 			}
 		}
-		function export_lessons_to_topics() {}
-		function export_enrollments() {}
+
+		function export_lessons_to_topics() {
+			$lessons = new WP_Query(
+				array(
+					'post_type'      => 'lesson',
+					'posts_per_page' => -1,
+				)
+			);
+			if ( $lessons && $lessons->post_count ) {
+				$lessons = $lessons->posts;
+				foreach ( $lessons as $lesson ) {
+					$post = array(
+						'wp_post'           => array(
+							'ID'                    => $lesson->ID,
+							'post_author'           => $lesson->post_author,
+							'post_date'             => $lesson->post_date,
+							'post_date_gmt'         => $lesson->post_date_gmt,
+							'post_content'          => $lesson->post_content,
+							'post_title'            => $lesson->post_title,
+							'post_excerpt'          => $lesson->post_excerpt,
+							'post_status'           => 'publish',
+							'comment_status'        => 'closed',
+							'ping_status'           => 'closed',
+							'post_password'         => '',
+							'post_name'             => $lesson->post_name,
+							'to_ping'               => '',
+							'pinged'                => '',
+							'post_modified'         => $lesson->post_modified,
+							'post_modified_gmt'     => $lesson->post_modified_gmt,
+							'post_content_filtered' => '',
+							'post_parent'           => 0,
+							'menu_order'            => $lesson->menu_order,
+							'post_type'             => 'sfwd-topic',
+							'post_mime_type'        => '',
+							'comment_count'         => '0',
+							'filter'                => 'raw',
+						),
+						'wp_post_permalink' => trailingslashit( home_url() . '/topics/' . $lesson->post_name ),
+						'wp_post_meta'      => array(
+							'course_id'   => array( wp_get_post_parent_id( wp_get_post_parent_id( $lesson->ID ) ) ),
+							'lesson_id'   => array( wp_get_post_parent_id( $lesson->ID ) ),
+							'_sfwd-topic' => array(
+								(object) array(
+									'0'                 => '',
+									'sfwd-topic_lesson' => wp_get_post_parent_id( $lesson->ID ), // parent (Tutor Topic)
+									'sfwd-topic_course' => wp_get_post_parent_id( wp_get_post_parent_id( $lesson->ID ) ), // ancestor (course)
+								),
+							),
+						),
+						'wp_post_terms'     => array(
+							'ld_topic_category' => array(),
+							'ld_topic_tag'      => array(),
+						),
+					);
+					$this->jlog( wp_json_encode( $post ), 'post_type_topic' );
+				}
+			}
+		}
+
+		function export_enrollments() {
+			$timestamp   = time();
+			$enrollments = new WP_Query(
+				array(
+					'post_type'      => 'tutor_enrolled',
+					'post_status'    => 'completed',
+					'posts_per_page' => -1,
+				)
+			);
+			if ( $enrollments && $enrollments->post_count ) {
+				$enrollments = $enrollments->posts;
+				foreach ( $enrollments as $enrollment ) {
+					$post = array(
+						'wp_post' => array(
+							'user_id'            => $enrollment->post_author,
+							'post_id'            => $enrollment->ID,
+							'course_id'          => wp_get_post_parent_id( $enrollment->ID ),
+							'activity_type'      => 'access',
+							'activity_status'    => '0',
+							'activity_started'   => strtotime( $enrollment->post_date_gmt ),
+							'activity_completed' => null,
+							'activity_updated'   => $timestamp,
+							'activity_meta'      => array(),
+						),
+					);
+					$this->jlog( wp_json_encode( $post ), 'user_activity' );
+				}
+			}
+
+		}
 
 		function convertDaysToWeeksMonthsAndYearsRounded( $days ) {
 			$daysPerYear  = 365;
